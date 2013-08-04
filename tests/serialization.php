0d0d4d9c6b29eeb30c762fad9b6401a88f0f799a
Jakub Zelenka <jakub.zelenka@gmail.com>
2013-08-04 19:55:42 +0100

    Add serialization examples

--- tests/serialization.php
+++ tests/serialization.php
@@ -1,4 +1,32 @@
 <?php
-$dt = new DateTime();
-print_r($dt);
-echo serialize($dt);
+echo "NULL\n";
+var_dump(serialize(null));
+echo "INT\n";
+var_dump(serialize(23));
+echo "DOUBLE\n";
+var_dump(serialize(23.2));
+var_dump(serialize(23.));
+echo "STRING\n";
+var_dump(serialize("test"));
+
+echo "ARRAY\n";
+var_dump(serialize(array(1, 2, 3)));
+var_dump(serialize(array("first" => "bum", "second" => "bam")));
+var_dump(serialize(array("first" => "bum", "nested" => array("bara", "kuba"))));
+
+echo "OBJECT: StdClass\n";
+$o = new StdClass;
+var_dump(serialize($o));
+$o->prop = "result";
+var_dump(serialize($o));
+
+echo "OBJECT: Custom\n";
+class Custom implements Serializable {
+	public function serialize() {
+		return "custom-datastr";
+	}
+	public function unserialize($str) {
+	}
+}
+$c = new Custom;
+var_dump(serialize($c));
