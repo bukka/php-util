f924fd1327c2212744f592ba31e77162d991a5ea
Jakub Zelenka <bukka@php.net>
2016-05-29 19:21:19 +0100

    Minimize script to recreate segfault after fann_destroy

--- tests/fann/repeate_fann_create_standard_segfault.php
+++ tests/fann/repeate_fann_create_standard_segfault.php
@@ -3,28 +3,11 @@ $num_input = 2;
 $num_output = 1;
 $num_layers = 3;
 $num_neurons_hidden = 3;
-$desired_error = 0.001;
-$max_neurons = 30;
-$neurons_between_reports = 1;
 $ann = fann_create_standard($num_layers, $num_input, $num_neurons_hidden, $num_output);
-$filename = ( dirname( __FILE__ ) . "/fann_cascadetrain_on_file.tmp" );				 
-$content = <<<EOF
-4 2 1
--1 -1
--1
--1 1
-1
-1 -1
-1
-1 1
--1
-EOF;
-file_put_contents( $filename, $content );
-var_dump($ann);
-fann_cascadetrain_on_file( $ann, $filename, $max_neurons, $neurons_between_reports, $desired_error ) ;
+// destroy is the key - there is no segfault without it
 fann_destroy($ann);
-
 $ann = fann_create_standard($num_layers, $num_input, $num_neurons_hidden, $num_output);
 var_dump($ann);
-var_dump( fann_cascadetrain_on_file( $ann, $filename, $max_neurons, $neurons_between_reports, $desired_error ) );
+// now any accesss will segfault
+var_dump(fann_get_errno($ann));
 
