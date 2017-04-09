03c100acedc0a3d83f74f31b067d441de086ddbf
Jakub Zelenka <bukka@php.net>
2017-04-09 20:05:38 +0100

    Update fpm loglimit test

--- tests/fpm/loglimit/test.php
+++ tests/fpm/loglimit/test.php
@@ -1,3 +1,3 @@
 <?php
-//error_log(str_repeat('a', 2048) . "\n");
-file_put_contents('php://stderr', str_repeat('a', 2048) . "\n");
+error_log(str_repeat('a', 2048));
+//file_put_contents('php://stderr', str_repeat('a', 2048) . "\n");
