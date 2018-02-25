37e666ba3e94937555ae5bd9593709a6674a5110
Jakub Zelenka <bukka@php.net>
2018-02-25 19:24:11 +0000

    Add fpm post test resources

---
+++ tests/fpm/post/curl.sh
@@ -0,0 +1,1 @@
+curl -X POST -H "Content-Type: application/json" -d '{"key1":"value1", "key2":"value2"}' http://localhost:8083/index.php
---
+++ tests/fpm/post/fpmt.sh
@@ -0,0 +1,1 @@
+fpmt client post -b '{"key1":"value1", "key2":"value2"}' -t "application/json" -s index.php 
