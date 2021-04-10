56bc2b77f7682b58726ecfeec5d06eb8d19d2b7a
Jakub Zelenka <bukka@php.net>
2021-04-10 19:06:55 +0100

    Update FPM status test

--- tests/fpm/status/test.php
+++ tests/fpm/status/test.php
@@ -1,5 +1,6 @@
 <?php
-error_log(str_repeat('a', 2048));
-file_put_contents('php://stderr', str_repeat('b', 2048) . "\n");
+//error_log(str_repeat('a', 2048));
+//file_put_contents('php://stderr', str_repeat('b', 2048) . "\n");
+
 
 echo "test";
