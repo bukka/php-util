92a02d92914821bd194867360d563ed5a462a80f
Jakub Zelenka <bukka@php.net>
2022-04-07 15:05:54 +0100

    Add FPM test / recreator for wrong active procs in status

---
+++ tests/fpm/status-wrong-active-procs/status-poll.php
@@ -0,0 +1,12 @@
+<?php
+
+while(true){
+    $json = file_get_contents("http://127.0.0.1:8083/status.php?json&full");
+    $result = json_decode($json);
+    if($result->{"active processes"} > 5){
+        echo "Value greater than max found!";
+        print_r($result);
+        echo "\n";
+        die();
+    }
+}
