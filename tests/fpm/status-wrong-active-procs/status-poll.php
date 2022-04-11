aa07dbe8075edfe8d03111bf81fc87654802403c
Jakub Zelenka <bukka@php.net>
2022-04-11 17:58:51 +0100

    Use more stable configuration to test FPM scoreboard race condition

--- tests/fpm/status-wrong-active-procs/status-poll.php
+++ tests/fpm/status-wrong-active-procs/status-poll.php
@@ -3,7 +3,7 @@
 while(true){
     $json = file_get_contents("http://127.0.0.1:8083/status.php?json&full");
     $result = json_decode($json);
-    if($result->{"active processes"} > 5){
+    if($result->{"active processes"} > 10) {
         echo "Value greater than max found!";
         print_r($result);
         echo "\n";
