27dfdcfda42c60e0eb36e6ef8367e371995a1995
Jakub Zelenka <bukka@php.net>
2023-11-16 15:26:53 +0000

    Enable GD on Mac

--- util/configure-mac.sh
+++ util/configure-mac.sh
@@ -1,3 +1,3 @@
-export PKG_CONFIG_PATH="/opt/homebrew/opt/openssl@1.1/lib/pkgconfig:/opt/homebrew/opt/libedit/lib/pkgconfig"
+export PKG_CONFIG_PATH="/opt/homebrew/opt/openssl@3/lib/pkgconfig:/opt/homebrew/opt/libedit/lib/pkgconfig"
 
-./configure --enable-cli --enable-fpm --enable-pcntl --with-openssl --with-libedit=/opt/homebrew/opt/libedit --with-zlib --with-iconv=/opt/homebrew/opt/libiconv
+./configure --enable-debug --enable-cli --enable-gd --enable-fpm --enable-pcntl --with-openssl --with-libedit=/opt/homebrew/opt/libedit --with-zlib --with-iconv=/opt/homebrew/opt/libiconv --with-gettext=/opt/homebrew/opt/gettext --without-pear
